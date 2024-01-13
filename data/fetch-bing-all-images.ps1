5aa490bd07c1e195bfc6166c23fc7a93058d7b92
Mykola Mozghovyi <mykola.mozghovyi@bolt.eu>
2024-01-13 23:25:14 +0200

    + fetch all bing food

--- data/fetch-bing-all-images.ps1
+++ data/fetch-bing-all-images.ps1
@@ -4,9 +4,16 @@
 
 Write-Output $BING_SEARCH_V7_KEY
 
-$allAnimals = cat "./eesti/animals.json" | ConvertFrom-Json
-$allAnimals | ForEach-Object {
-    $animal = $_.en
-    Write-Output $animal
-    ./fetch-bing-image.ps1 $BING_SEARCH_V7_KEY "animal" $animal
+# $allAnimals = cat "./eesti/animals.json" | ConvertFrom-Json
+# $allAnimals | ForEach-Object {
+#     $animal = $_.en
+#     Write-Output $animal
+#     ./fetch-bing-image.ps1 $BING_SEARCH_V7_KEY "animal" $animal
+# }
+
+$allFoods = cat "./eesti/food.json" | ConvertFrom-Json
+$allFoods | ForEach-Object {
+    $food = $_.en
+    Write-Output $food
+    ./fetch-bing-image.ps1 $BING_SEARCH_V7_KEY "food" $food
 }
