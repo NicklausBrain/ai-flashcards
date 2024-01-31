a06165ef1b46ba671865bc634e754829f88ace5b
Mykola Mozghovyi <mykola.mozghovyi@bolt.eu>
2024-01-31 09:07:00 +0200

    + test useState

--- react-app/App.js
+++ react-app/App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const title = `Example button ${count}`;
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title={title}
+        onPress={() => {
+          setCount(count + 1);
+        }}
+      ></Button>
     </View>
   );
 }
