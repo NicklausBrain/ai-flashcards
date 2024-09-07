30385718cccea439c7e8b6c0ab9cf406d6a369d8
Mykola Mozghovyi <mykola.mozghovyi@bolt.eu>
2024-09-07 21:50:29 +0300

    + AddEnvironmentVariables

--- My1kWordsEe/Program.cs
+++ My1kWordsEe/Program.cs
@@ -8,6 +8,7 @@ namespace My1kWordsEe
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Configuration.AddEnvironmentVariables();
 
             var openApiKey = builder.Configuration["Secrets:OpenAiKey"];
 
