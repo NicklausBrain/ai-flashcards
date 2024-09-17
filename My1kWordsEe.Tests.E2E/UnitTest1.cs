4ab3714e2d7932c887ef635109f9f8f223b9f63f
NICK BRAIN <nicklausbrain@gmail.com>
2024-09-17 14:18:28 +0300

    + e2e test proj

---
+++ My1kWordsEe.Tests.E2E/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace My1kWordsEe.Tests.E2E;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
