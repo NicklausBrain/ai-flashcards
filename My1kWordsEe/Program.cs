238956a3ba765edfa51951ab98c6e40d8e61bc2b
NICK BRAIN <nicklausbrain@gmail.com>
2024-09-20 13:10:28 +0300

    tests pass

--- My1kWordsEe/Program.cs
+++ My1kWordsEe/Program.cs
@@ -70,7 +70,6 @@ namespace My1kWordsEe
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-                app.UseResponseCompression();
             }
 
             app.UseHttpsRedirection();
