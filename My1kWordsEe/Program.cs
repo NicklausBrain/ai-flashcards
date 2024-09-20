b1bf4b07fa80b6f6a50ff289a9a36c40e0afe9ad
NICK BRAIN <nicklausbrain@gmail.com>
2024-09-20 11:14:05 +0300

    + compression & timeouts

--- My1kWordsEe/Program.cs
+++ My1kWordsEe/Program.cs
@@ -48,7 +48,12 @@ namespace My1kWordsEe
             // Blazor-specific services
             builder.Services
                 .AddRazorComponents()
-                .AddInteractiveServerComponents();
+                .AddInteractiveServerComponents()
+                .AddHubOptions(options =>
+                {
+                    options.ClientTimeoutInterval = TimeSpan.FromSeconds(7);
+                    options.HandshakeTimeout = TimeSpan.FromSeconds(14);
+                });
 
             var app = builder.Build();
 
@@ -58,6 +63,7 @@ namespace My1kWordsEe
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseResponseCompression();
             }
 
             app.UseHttpsRedirection();
