6760eec22d2f627d10b4701d6c3fe7250ce46169
Mykola Mozghovyi <nicklausbrain@gmail.com>
2024-10-02 11:17:41 +0300

    search english word (#51)
    * search english word
    * translate autoformat off

--- My1kWordsEe/Models/Ee1kWords.cs
+++ My1kWordsEe/Models/Ee1kWords.cs
@@ -24,7 +24,8 @@ namespace My1kWordsEe.Models
             {
                 Search = search;
                 SelectedWords = AllWords.Where(w =>
-                    AllWordsDiacriticsFree[w.Value].Contains(search, StringComparison.InvariantCultureIgnoreCase));
+                    AllWordsDiacriticsFree[w.Value].Contains(search, StringComparison.InvariantCultureIgnoreCase) ||
+                    w.EnWords.Any(en => en.Contains(search, StringComparison.InvariantCultureIgnoreCase)));
             }
             else
             {
