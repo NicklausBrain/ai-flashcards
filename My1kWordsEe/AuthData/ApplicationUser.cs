bf105015008081dceeadbb5e4a47de675e048def
Mykola Mozghovyi <nicklausbrain@gmail.com>
2024-11-05 00:13:33 +0200

    + UseCosmos (#63)
    * + UseCosmos
    * + Secrets__AzureCosmosConnectionString
    * - extra packages

--- My1kWordsEe/AuthData/ApplicationUser.cs
+++ My1kWordsEe/AuthData/ApplicationUser.cs
@@ -5,6 +5,7 @@ namespace My1kWordsEe.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+
     }
 
 }
--- auth-sample/BlazorAppAuth/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace BlazorAppAuth.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
