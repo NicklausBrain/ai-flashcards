25e7164968a3d38eb52d17de0916ccff3013d76e
NICK BRAIN <nicklausbrain@gmail.com>
2024-11-23 17:03:27 +0200

    + GetFavoritesCommand

---
+++ My1kWordsEe/Models/Favorite.cs
@@ -0,0 +1,18 @@
+﻿namespace My1kWordsEe.Models
+{
+    public class FavoriteWord
+    {
+        public required string EeWord { get; set; }
+
+        public required string EnWord { get; init; }
+    }
+
+    public class Favorites
+    {
+        public required string UserId { get; set; }
+
+        public List<FavoriteWord> Words { get; set; } = new List<FavoriteWord>();
+
+        // to be added: Phrases
+    }
+}
---
+++ My1kWordsEe/Services/Db/AzureFavoritesStorage.cs
@@ -0,0 +1,64 @@
+﻿using System.Text.Json;
+
+using Azure;
+using Azure.Storage.Blobs;
+
+using CSharpFunctionalExtensions;
+
+using My1kWordsEe.Models;
+
+namespace My1kWordsEe.Services.Db
+{
+    public partial class AzureStorageClient
+    {
+        public async Task<Result<Maybe<Favorites>>> GetFavorites(string userId)
+        {
+            var container = await GetFavoritesContainer();
+
+            if (container.IsFailure)
+            {
+                return Result.Failure<Maybe<Favorites>>(container.Error);
+            }
+
+            BlobClient blob = container.Value.GetBlobClient(JsonBlobName(userId));
+
+            if (!await blob.ExistsAsync())
+            {
+                return Maybe<Favorites>.None;
+            }
+
+            try
+            {
+                var response = await blob.DownloadContentAsync();
+                if (response != null && response.HasValue)
+                {
+                    var favorites = JsonSerializer.Deserialize<Favorites>(response.Value.Content);
+                    return Maybe<Favorites>.From(favorites);
+                }
+                else
+                {
+                    return Maybe<Favorites>.None;
+                }
+            }
+            catch (RequestFailedException ex)
+            {
+                this.logger.LogError(ex, "Failure to download data from blob {name}", blob.Name);
+                return Result.Failure<Maybe<Favorites>>("Failure to download data from blob");
+            }
+            catch (Exception ex) when (ex is JsonException || ex is NotSupportedException)
+            {
+                this.logger.LogError(ex, "Failure to parse JSON from blob {name}", blob.Name);
+                return Result.Failure<Maybe<Favorites>>("Failure to parse JSON data from blob");
+            }
+        }
+
+        public Task<Result<Uri>> SaveFavorites(Favorites favorites) =>
+            this.GetFavoritesContainer().Bind(container =>
+            this.UploadStreamAsync(
+                container.GetBlobClient(JsonBlobName(favorites.UserId)),
+                new MemoryStream(JsonSerializer.SerializeToUtf8Bytes(favorites))));
+
+        private Task<Result<BlobContainerClient>> GetFavoritesContainer() =>
+            this.GetOrCreateContainer("favorites");
+    }
+}
