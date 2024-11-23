6af8f3dab8898beffc3334c16550e67a34672d37
NICK BRAIN <nicklausbrain@gmail.com>
2024-11-23 14:21:15 +0200

    + extracted case-specific storage logic

---
+++ My1kWordsEe/Services/Db/AzureAudioStorage.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Storage.Blobs;
+
+using CSharpFunctionalExtensions;
+
+namespace My1kWordsEe.Services.Db
+{
+    public partial class AzureStorageClient
+    {
+        public Task<Result<bool>> DeleteAudio(string blobName) =>
+            this.GetAudioContainer().Bind((container) =>
+            this.DeleteIfExistsAsync(container.GetBlobClient(blobName)));
+
+        public Task<Result<Uri>> SaveAudio(Stream audioStream, string blobName) =>
+            this.GetAudioContainer().Bind((container) =>
+            this.UploadStreamAsync(
+                container.GetBlobClient(blobName),
+                audioStream));
+
+        public Task<Result<Uri>> SaveAudio(Stream audioStream) =>
+            this.SaveAudio(audioStream, WavBlobName());
+
+        private Task<Result<BlobContainerClient>> GetAudioContainer() =>
+            this.GetOrCreateContainer("audio");
+
+        private static string WavBlobName() => Guid.NewGuid() + ".wav";
+    }
+}
---
+++ My1kWordsEe/Services/Db/AzureImageStorage.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Storage.Blobs;
+
+using CSharpFunctionalExtensions;
+
+namespace My1kWordsEe.Services.Db
+{
+    public partial class AzureStorageClient
+    {
+        public Task<Result<bool>> DeleteImage(string blobName) =>
+            this.GetImageContainer().Bind((container) =>
+            this.DeleteIfExistsAsync(container.GetBlobClient(blobName)));
+
+        public Task<Result<Uri>> SaveImage(Stream imageStream) =>
+            this.GetImageContainer().Bind((container) =>
+            this.UploadStreamAsync(
+                container.GetBlobClient(JpgBlobName()),
+                imageStream));
+
+        private Task<Result<BlobContainerClient>> GetImageContainer() =>
+            this.GetOrCreateContainer("image");
+
+        private static string JpgBlobName() => Guid.NewGuid() + ".jpg";
+    }
+}
