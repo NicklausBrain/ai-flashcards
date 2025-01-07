d61fcbebb9b07a6caa25cb25a1db218134e4de57
Mykola Mozghovyi <nicklausbrain@gmail.com>
2025-01-07 18:56:39 +0200

    Game unit tests (#165)
    * + TranslateToEnGameTest
    * + ListenToEeGameTest
    * + Word2WordMatchGameTest

---
+++ My1kWordsEe.Tests.Unit/Games/Word2WordMatchGameTest.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using My1kWordsEe.Models.Games;
+using Xunit;
+
+namespace My1kWordsEe.Tests.Models.Games
+{
+    public class Word2WordMatchGameTest
+    {
+        [Fact]
+        public void Constructor_ShouldInitializeProperties()
+        {
+            var pairs = new List<Word2WordMatchGame.Pair>
+            {
+                new Word2WordMatchGame.Pair { EeWord = "Tere", EnWord = "Hello" },
+                new Word2WordMatchGame.Pair { EeWord = "Kass", EnWord = "Cat" }
+            };
+            var eeWords = pairs.ToDictionary(p => p.EeWord);
+            var enWords = pairs.ToDictionary(p => p.EnWord);
+
+            var game = new Word2WordMatchGame(pairs, eeWords, enWords);
+
+            Assert.Equal(eeWords, game.EeWords);
+            Assert.Equal(enWords, game.EnWords);
+            Assert.Equivalent(eeWords.Keys, game.EeWords2Match);
+            Assert.Equivalent(enWords.Keys, game.EnWords2Match);
+            Assert.True(game.IsReady);
+            Assert.False(game.IsFinished);
+        }
+
+        [Fact]
+        public void TryMatch_ShouldReturnTrue_WhenWordsMatch()
+        {
+            var pair = new Word2WordMatchGame.Pair { EeWord = "Tere", EnWord = "Hello" };
+            var pairs = new List<Word2WordMatchGame.Pair> { pair };
+            var eeWords = pairs.ToDictionary(p => p.EeWord);
+            var enWords = pairs.ToDictionary(p => p.EnWord);
+
+            var game = new Word2WordMatchGame(pairs, eeWords, enWords);
+
+            var result = game.TryMatch("Tere", "Hello");
+
+            Assert.True(result);
+            Assert.Contains(pair, game.Matches);
+            Assert.DoesNotContain("Tere", game.EeWords2Match);
+            Assert.DoesNotContain("Hello", game.EnWords2Match);
+            Assert.True(pair.IsMatched);
+        }
+
+        [Fact]
+        public void TryMatch_ShouldReturnFalse_WhenWordsDoNotMatch()
+        {
+            var pair = new Word2WordMatchGame.Pair { EeWord = "Tere", EnWord = "Hello" };
+            var pairs = new List<Word2WordMatchGame.Pair> { pair };
+            var eeWords = pairs.ToDictionary(p => p.EeWord);
+            var enWords = pairs.ToDictionary(p => p.EnWord);
+
+            var game = new Word2WordMatchGame(pairs, eeWords, enWords);
+
+            var result = game.TryMatch("Tere", "Cat");
+
+            Assert.False(result);
+            Assert.DoesNotContain(pair, game.Matches);
+            Assert.Contains("Tere", game.EeWords2Match);
+            Assert.DoesNotContain("Cat", game.EnWords2Match);
+            Assert.False(pair.IsMatched);
+        }
+
+        [Fact]
+        public void IsReady_ShouldReturnTrue_WhenPairsExist()
+        {
+            var pairs = new List<Word2WordMatchGame.Pair>
+            {
+                new Word2WordMatchGame.Pair { EeWord = "Tere", EnWord = "Hello" }
+            };
+            var eeWords = pairs.ToDictionary(p => p.EeWord);
+            var enWords = pairs.ToDictionary(p => p.EnWord);
+
+            var game = new Word2WordMatchGame(pairs, eeWords, enWords);
+
+            Assert.True(game.IsReady);
+        }
+
+        [Fact]
+        public void IsFinished_ShouldReturnTrue_WhenAllPairsAreMatched()
+        {
+            var pairs = new List<Word2WordMatchGame.Pair>
+            {
+                new Word2WordMatchGame.Pair { EeWord = "Tere", EnWord = "Hello", IsMatched = true }
+            };
+            var eeWords = pairs.ToDictionary(p => p.EeWord);
+            var enWords = pairs.ToDictionary(p => p.EnWord);
+
+            var game = new Word2WordMatchGame(pairs, eeWords, enWords);
+
+            Assert.True(game.IsFinished);
+        }
+
+        [Fact]
+        public async Task Generate_ShouldCreateGameWithCorrectNumberOfPairs()
+        {
+            var game = await Word2WordMatchGame.Generate();
+
+            Assert.Equal(5, game.EeWords.Count);
+            Assert.Equal(5, game.EnWords.Count);
+            Assert.True(game.IsReady);
+            Assert.False(game.IsFinished);
+        }
+    }
+}
--- My1kWordsEe/Models/Games/TranslateToEnGame.cs
+++ My1kWordsEe/Models/Games/TranslateToEnGame.cs
@@ -4,57 +4,6 @@ using My1kWordsEe.Services.Cqs;
 
 namespace My1kWordsEe.Models.Games
 {
-    public class TranslateToEnGameFactory
-    {
-        GetOrAddSampleWordCommand getOrAddSampleWordCommand;
-        AddSampleSentenceCommand addSampleSentenceCommand;
-        CheckEnTranslationCommand checkEnTranslationCommand;
-
-        public TranslateToEnGameFactory(
-            GetOrAddSampleWordCommand getOrAddSampleWordCommand,
-            AddSampleSentenceCommand addSampleSentenceCommand,
-            CheckEnTranslationCommand checkEnTranslationCommand)
-        {
-            this.getOrAddSampleWordCommand = getOrAddSampleWordCommand;
-            this.addSampleSentenceCommand = addSampleSentenceCommand; ;
-            this.checkEnTranslationCommand = checkEnTranslationCommand;
-        }
-
-        public async Task<Result<TranslateToEnGame>> Generate(string? eeWord, int? wordIndex)
-        {
-            eeWord = (eeWord ?? GetRandomEeWord()).ToLower();
-            var sampleWord = await getOrAddSampleWordCommand.Invoke(eeWord);
-
-            if (sampleWord.IsFailure)
-            {
-                return Result.Failure<TranslateToEnGame>(sampleWord.Error);
-            }
-
-            if (sampleWord.Value.Samples.Any())
-            {
-                return new TranslateToEnGame(eeWord, 0, sampleWord.Value.Samples.First(), this.checkEnTranslationCommand);
-            }
-            else
-            {
-                var addSampleResult = await addSampleSentenceCommand.Invoke(sampleWord.Value);
-
-                if (addSampleResult.IsSuccess)
-                {
-                    return new TranslateToEnGame(eeWord, 0, addSampleResult.Value.Samples.First(), this.checkEnTranslationCommand);
-                }
-
-                return Result.Failure<TranslateToEnGame>(addSampleResult.Error);
-            }
-        }
-
-        private static string GetRandomEeWord()
-        {
-            var rn = new Random(Environment.TickCount);
-            var eeWord = Ee1kWords.AllWords[rn.Next(0, Ee1kWords.AllWords.Length)];
-            return eeWord.EeWord;
-        }
-    }
-
     public class TranslateToEnGame
     {
         private readonly CheckEnTranslationCommand checkEnTranslationCommand;
