1af7f059302bede6938ca015f0dc114ae0f1e554
Mykola Mozghovyi <nicklausbrain@gmail.com>
2025-02-28 15:50:43 +0200

    Translate to et (#205)
    * + CheckEtTranslationCommand
    * + TranslateToEtGamePage
    * + audio feedback

--- My1kWordsEe/Models/Games/Generation/TranslateToEtGameFactory.cs
+++ My1kWordsEe/Models/Games/Generation/TranslateToEtGameFactory.cs
@@ -6,30 +6,30 @@ using My1kWordsEe.Services.Cqs.Et;
 
 namespace My1kWordsEe.Models.Games
 {
-    public class TranslateToEnGameFactory
+    public class TranslateToEtGameFactory
     {
         private readonly NextWordSelector nextWordSelector;
         private readonly GetOrAddEtWordCommand getOrAddEtWordCommand;
         private readonly GetEtSampleSentencesQuery getEtSampleSentencesQuery;
         private readonly AddEtSampleSentenceCommand addEtSampleSentenceCommand;
-        private readonly CheckEnTranslationCommand checkEnTranslationCommand;
+        private readonly CheckEtTranslationCommand checkEtTranslationCommand;
 
-        public TranslateToEnGameFactory(
+        public TranslateToEtGameFactory(
             NextWordSelector nextWordSelector,
             GetOrAddEtWordCommand getOrAddEtWordCommand,
             GetEtSampleSentencesQuery getEtSampleSentencesQuery,
             AddEtSampleSentenceCommand addEtSampleSentenceCommand,
-            CheckEnTranslationCommand checkEnTranslationCommand)
+            CheckEtTranslationCommand checkEtTranslationCommand)
         {
             this.nextWordSelector = nextWordSelector;
             this.getOrAddEtWordCommand = getOrAddEtWordCommand;
             this.getEtSampleSentencesQuery = getEtSampleSentencesQuery;
             this.addEtSampleSentenceCommand = addEtSampleSentenceCommand;
-            this.checkEnTranslationCommand = checkEnTranslationCommand;
+            this.checkEtTranslationCommand = checkEtTranslationCommand;
         }
 
         // todo checl eeWord naming
-        public async Task<Result<TranslateToEnGame>> Generate(string? eeWord, int? wordIndex)
+        public async Task<Result<TranslateToEtGame>> Generate(string? eeWord, int? wordIndex)
         {
             const int senseIndex = 0;
             eeWord = (eeWord ?? await GetRandomEeWord()).ToLower();
@@ -37,19 +37,19 @@ namespace My1kWordsEe.Models.Games
 
             if (etWord.IsFailure)
             {
-                return Result.Failure<TranslateToEnGame>(etWord.Error);
+                return Result.Failure<TranslateToEtGame>(etWord.Error);
             }
 
             var samples = await getEtSampleSentencesQuery.Invoke(etWord.Value, senseIndex);
 
             if (samples.IsFailure)
             {
-                return Result.Failure<TranslateToEnGame>(samples.Error);
+                return Result.Failure<TranslateToEtGame>(samples.Error);
             }
 
             if (samples.Value.Any())
             {
-                return new TranslateToEnGame(eeWord, 0, samples.Value.First(), this.checkEnTranslationCommand);
+                return new TranslateToEtGame(eeWord, 0, samples.Value.First(), this.checkEtTranslationCommand);
             }
             else
             {
@@ -57,10 +57,10 @@ namespace My1kWordsEe.Models.Games
 
                 if (addSampleResult.IsSuccess)
                 {
-                    return new TranslateToEnGame(eeWord, 0, addSampleResult.Value.First(), this.checkEnTranslationCommand);
+                    return new TranslateToEtGame(eeWord, 0, addSampleResult.Value.First(), this.checkEtTranslationCommand);
                 }
 
-                return Result.Failure<TranslateToEnGame>(addSampleResult.Error);
+                return Result.Failure<TranslateToEtGame>(addSampleResult.Error);
             }
         }
 
---
+++ My1kWordsEe/Services/Cqs/CheckEtTranslationCommand.cs
@@ -0,0 +1,90 @@
+using System.ComponentModel;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using CSharpFunctionalExtensions;
+
+using My1kWordsEe.Models;
+
+namespace My1kWordsEe.Services.Cqs
+{
+    public class CheckEtTranslationCommand
+    {
+        public static readonly string Prompt =
+$@"Sinu ülesandeks on kontrollida õpilase tõlget inglise keelest eesti keelde.
+Ignoreeri oma tšekis suurtähti (ülemine või alumine) ja kirjavahemärke.
+Teie sisend on JSON-objekt:
+{JsonSchemaRecord.For(typeof(Input))}";
+
+        private readonly OpenAiClient openAiClient;
+
+        public CheckEtTranslationCommand(OpenAiClient openAiClient)
+        {
+            this.openAiClient = openAiClient;
+        }
+
+        public virtual async Task<Result<EtTranslationCheckResult>> Invoke(string etSentence, string enSentence)
+        {
+            var input = JsonSerializer.Serialize(new Input
+            {
+                EnSentence = enSentence.Trim('.', ' ').ToLowerInvariant(),
+                EtUserSentence = etSentence.Trim('.', ' ').ToLowerInvariant()
+            });
+
+            var result = await this.openAiClient.CompleteJsonSchemaAsync<EtTranslationCheckResult>(
+                instructions: Prompt,
+                input: input,
+                schema: JsonSchemaRecord.For(typeof(EtTranslationCheckResult)));
+
+            return result;
+        }
+
+        public struct Input
+        {
+            [Description("The sentence shown to the user for translation to Estonian")]
+            public string EnSentence { get; init; }
+
+            [Description("Kasutaja poolt tehtud tõlge eesti keelde")]
+            public string EtUserSentence { get; init; }
+        }
+    }
+
+    public struct EtTranslationCheckResult
+    {
+        [Description("The sentence shown to the user for translation to Estonian")]
+        public required string EnSentence { get; init; }
+
+        [Description("Kasutaja poolt tehtud tõlge eesti keelde")]
+        public required string EtUserSentence { get; init; }
+
+        [Description("Eeldatav eestikeelne lause")]
+        public required string EtExpectedSentence { get; init; }
+
+        [Description("Comment explaining to the student his mistake (if any) in English")]
+        public required string EnComment { get; init; }
+
+        [Description("Correctes level in integer from 0 to 5")]
+        public required ushort Match { get; init; }
+
+        [JsonIgnore]
+        public bool IsMaxMatch => this.Match == 5;
+
+        public static EtTranslationCheckResult Success(string enSentence, string etSentence) => new EtTranslationCheckResult
+        {
+            EnSentence = enSentence,
+            EtUserSentence = enSentence,
+            EtExpectedSentence = etSentence,
+            Match = 5,
+            EnComment = string.Empty
+        };
+
+        public static EtTranslationCheckResult Fail(string enSentence, string etSentence) => new EtTranslationCheckResult
+        {
+            EnSentence = enSentence,
+            EtUserSentence = "",
+            EtExpectedSentence = etSentence,
+            Match = 0,
+            EnComment = string.Empty
+        };
+    }
+}
