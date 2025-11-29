b3b4054ea79f81a35b1217d4abd5de612b61aa0e
Mykola Mozghovyi <mykola.mozghovyi@bolt.eu>
2025-11-29 19:29:22 +0200

    - old blazor app

--- blazor-app/Core/FlashCard.cs
+++
@@ -1,9 +0,0 @@
-namespace blazor_app.Core
-{
-    public class FlashCard
-    {
-        public string En { get; set; }
-        public string Et { get; set; }
-        public string ImgUrl { get; set; }
-    }
-}
