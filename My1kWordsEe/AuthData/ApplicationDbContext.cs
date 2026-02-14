2a0c865530d0ae1d069123cfd56cff07847d01a5
Mykola Mozghovyi <nicklausbrain@gmail.com>
2026-02-14 22:53:26 +0200

    + update the packages onto last stable (#259)

--- My1kWordsEe/AuthData/ApplicationDbContext.cs
+++ My1kWordsEe/AuthData/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace My1kWordsEe.Data
 {
@@ -9,12 +10,35 @@ namespace My1kWordsEe.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            RemoveIdentityIndexes(builder);
             builder.Entity<IdentityRole>()
                 .Property(b => b.ConcurrencyStamp)
                 .IsETagConcurrency();
             builder.Entity<ApplicationUser>() // ApplicationUser mean the Identity user 'ApplicationUser : IdentityUser'
                 .Property(b => b.ConcurrencyStamp)
                 .IsETagConcurrency();
+        }
+
+        private static void RemoveIdentityIndexes(ModelBuilder builder)
+        {
+            var roleEntity = builder.Entity<IdentityRole>();
+            RemoveIndex(roleEntity, nameof(IdentityRole.NormalizedName));
+
+            var userEntity = builder.Entity<ApplicationUser>();
+            RemoveIndex(userEntity, nameof(IdentityUser.NormalizedUserName));
+            RemoveIndex(userEntity, nameof(IdentityUser.NormalizedEmail));
+        }
+
+        private static void RemoveIndex<TEntity>(EntityTypeBuilder<TEntity> entity, string propertyName)
+            where TEntity : class
+        {
+            var property = entity.Property(propertyName).Metadata;
+            var index = entity.Metadata.FindIndex(new[] { property });
+            if (index != null)
+            {
+                entity.Metadata.RemoveIndex(index);
+            }
         }
     }
 }
